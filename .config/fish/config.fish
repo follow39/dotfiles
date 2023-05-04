5a5ae7fcf593ca729ba79bad8ae052dd4a41cb4f
follow39 <ingang39@gmail.com>
2023-05-04 18:35:52 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
+
+    export EDITOR='emacsclient -t -n -a ""'
+    alias ec='emacsclient -c -n -a ""'
+    alias et='emacsclient -t -a ""'
 end
